35613b8d757d1af443cc3b2a1873de353469b9f8
Mickael Pham <mickael.pham@outlook.com>
2013-11-09 18:25:37 -0800

    Changed test

--- src/test/java/eu/mpham/linkedlist/impl/LinkedListImplTest.java
+++ src/test/java/eu/mpham/linkedlist/impl/LinkedListImplTest.java
@@ -1,4 +1,4 @@
-package eu.mpham.linkedlist;
+package eu.mpham.linkedlist.impl;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -7,32 +7,28 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class LinkedListImplTest extends TestCase {
+	/**
+	 * Create the test case
+	 * 
+	 * @param testName
+	 *            name of the test case
+	 */
+	public LinkedListImplTest(String testName) {
+		super(testName);
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(LinkedListImplTest.class);
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testApp() {
+		assertTrue(true);
+	}
 }
